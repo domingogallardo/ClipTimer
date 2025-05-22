16d5439c4db079b79a0550de55c31f3cf4708a8e
Domingo Gallardo <domingo.gallardo@ua.es>
2025-05-22 13:09:41 +0200

    Initial Commit

---
+++ ClipTimer/ClipTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipTimerApp.swift
+//  ClipTimer
+//
+//  Created by Domingo Gallardo on 22/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClipTimer
+//
+//  Created by Domingo Gallardo on 22/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
